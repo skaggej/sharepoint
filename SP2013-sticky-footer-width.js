8699d2776783991311e440a8c4b16d8dcffb87f4
skaggej <eskaggs@outlook.com>
2016-04-14 17:36:38 -0700

    updated

--- SP2013-sticky-footer-width.js
+++ SP2013-sticky-footer-width.js
@@ -1,11 +1,11 @@
 function setFooterWidth() {
 	//Retrieve the width from list view table
 	var tableWidth = parseInt($("#WebPartWPQ2").width());
-	if(tableWidth > parseInt($("body").width())) {
-		//Retrieve the left margin for the quicklaunch
-		var leftMargin = $("#contentBox").css("margin-left").replace("px", "");
+	//Retrieve the left margin for the quicklaunch
+	var leftMargin = $("#contentBox").css("margin-left").replace("px", "");
+	if(parseInt(tableWidth)+parseInt(leftMargin) > parseInt($("body").width())) {
 		var newSize = tableWidth + parseInt(leftMargin);
-		$("customFooter").width(newSize);
+		$("#customFooter").width(newSize);
 	}		
 }
 $(document).ready(function() {
