4a96db8a487f6295849291f32fa7c0e50e38043a
skaggej <eskaggs@outlook.com>
2016-04-18 10:30:17 -0700

    Added

---
+++ SampleAddIn/SampleAddInWeb/Pages/Default.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SampleAddInWeb.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+      <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"
+                EnablePartialRendering="true" />
+        <asp:UpdatePanel ID="PopulateData" runat="server" UpdateMode="Conditional">
+          <ContentTemplate>      
+            <table border="1" cellpadding="10">
+             <tr><th><asp:LinkButton ID="CSOM" runat="server" Text="Populate Data" 
+                                   OnClick="CSOM_Click" /></th></tr>
+             <tr><td>
+
+            <h2>SharePoint Site</h2>
+            <asp:Label runat="server" ID="WebTitleLabel"/>
+
+            <h2>Current User:</h2>
+            <asp:Label runat="server" ID="CurrentUserLabel" />
+
+            <h2>Site Users</h2>
+            <asp:ListView ID="UserList" runat="server">     
+                <ItemTemplate >
+                  <asp:Label ID="UserItem" runat="server" 
+                                    Text="<%# Container.DataItem.ToString()  %>">
+                  </asp:Label><br />
+               </ItemTemplate>
+            </asp:ListView>
+
+            <h2>Site Lists</h2>
+                   <asp:ListView ID="ListList" runat="server">
+                       <ItemTemplate >
+                         <asp:Label ID="ListItem" runat="server" 
+                                    Text="<%# Container.DataItem.ToString()  %>">
+                        </asp:Label><br />
+                      </ItemTemplate>
+                  </asp:ListView>
+                </td>              
+              </tr>
+             </table>
+           </ContentTemplate>
+         </asp:UpdatePanel>
+      </div>
+    </form>
+</body>
+</html>
---
+++ SampleAddIn/SampleAddInWeb/Pages/Default.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+using Microsoft.SharePoint.Client;
+
+namespace SampleAddInWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        SharePointContextToken contextToken;
+        string accessToken;
+        Uri sharepointUrl;
+        string siteName;
+        string currentUser;
+        List<string> listOfUsers = new List<string>();
+        List<string> listOfLists = new List<string>();
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            Uri redirectUrl;
+            switch (SharePointContextProvider.CheckRedirectionStatus(Context, out redirectUrl))
+            {
+                case RedirectionStatus.Ok:
+                    return;
+                case RedirectionStatus.ShouldRedirect:
+                    Response.Redirect(redirectUrl.AbsoluteUri, endResponse: true);
+                    break;
+                case RedirectionStatus.CanNotRedirect:
+                    Response.Write("An error occurred while processing your request.");
+                    Response.End();
+                    break;
+            }
+        }
+
+        // The Page_load method fetches the context token and the access token. 
+        // The access token is used by all of the data retrieval methods.
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string contextTokenString = TokenHelper.GetContextTokenFromRequest(Request);
+
+            if (contextTokenString != null)
+            {
+                contextToken =
+                    TokenHelper.ReadAndValidateContextToken(contextTokenString, Request.Url.Authority);
+
+                sharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+                accessToken =
+                            TokenHelper.GetAccessToken(contextToken, sharepointUrl.Authority)
+                            .AccessToken;
+
+                // For simplicity, this sample assigns the access token to the button's CommandArgument property. 
+                // In a production add-in, this would not be secure. The access token should be cached on the server-side.
+                CSOM.CommandArgument = accessToken;
+            }
+            else if (!IsPostBack)
+            {
+                Response.Write("Could not find a context token.");
+                return;
+            }
+        }
+
+        // This method retrieves information about the host web by using the CSOM.
+        private void RetrieveWithCSOM(string accessToken)
+        {
+
+            if (IsPostBack)
+            {
+                sharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+            }
+
+            ClientContext clientContext =
+                            TokenHelper.GetClientContextWithAccessToken(
+                                sharepointUrl.ToString(), accessToken);
+
+            // Load the properties for the web object.
+            Web web = clientContext.Web;
+            clientContext.Load(web);
+            clientContext.ExecuteQuery();
+
+            // Get the site name.
+            siteName = web.Title;
+
+            // Get the current user.
+            clientContext.Load(web.CurrentUser);
+            clientContext.ExecuteQuery();
+            currentUser = clientContext.Web.CurrentUser.LoginName;
+
+            // Load the lists from the Web object.
+            ListCollection lists = web.Lists;
+            clientContext.Load<ListCollection>(lists);
+            clientContext.ExecuteQuery();
+
+            // Load the current users from the Web object.
+            UserCollection users = web.SiteUsers;
+            clientContext.Load<UserCollection>(users);
+            clientContext.ExecuteQuery();
+
+            foreach (User siteUser in users)
+            {
+                listOfUsers.Add(siteUser.LoginName);
+            }
+
+
+            foreach (List list in lists)
+            {
+                listOfLists.Add(list.Title);
+            }
+        }
+
+        protected void CSOM_Click(object sender, EventArgs e)
+        {
+            string commandAccessToken = ((LinkButton)sender).CommandArgument;
+            RetrieveWithCSOM(commandAccessToken);
+            WebTitleLabel.Text = siteName;
+            CurrentUserLabel.Text = currentUser;
+            UserList.DataSource = listOfUsers;
+            UserList.DataBind();
+            ListList.DataSource = listOfLists;
+            ListList.DataBind();
+        }
+    }
+}
